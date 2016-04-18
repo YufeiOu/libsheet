33afe392cd4f6420d64fe79499dd9af50883b85e
Mingching <mingchingchu@gmail.com>
2016-04-17 22:51:56 -0400

    can load file

---
+++ libsheet.cpp
@@ -0,0 +1,144 @@
+// libsheet.cpp
+#include "libsheet.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <exception>
+#include <cassert>
+#include <sstream>
+using namespace std;
+
+string trim(string& str)
+{
+	if (!str.size()) return str;
+	size_t first = str.find_first_not_of(' ');
+	size_t last = str.find_last_not_of(' ');
+	return str.substr(first, (last-first+1));
+}
+
+int get_type(const string& input) {
+	try{
+		// First check if it's int
+		int a = stoi(input);
+		if (to_string(a).size() != input.size()) {
+			throw invalid_argument("Not valid int");
+		} else {
+			return 0;
+		}
+	}
+	catch(invalid_argument& e) {
+		bool found_dot = false;
+		for (string::const_iterator i = input.begin(); i != input.end(); ++i) {
+			if (!isdigit(*i)) {
+				if (*i == '.' && !found_dot) {
+					found_dot = true;
+				} else return 2; // it's a string
+			}
+		}
+		return found_dot ? 1 : 2;
+	}
+}
+
+Sheet::Sheet(vector<string>& entry, vector<string>& col_names) {
+	assert(entry.size() == col_names.size()); // assume every entry is provided a column name
+	auto j = col_names.begin();
+	for (auto i = entry.begin(); i != entry.end(); ++i, ++j) {
+		int type = get_type(*i);
+		ColumnHead ch(*j, type);
+		auto result = column_map.insert(pair<string, unsigned int>(*j, i - entry.begin()));
+		if (!result.second) {
+			throw "Duplicate column name at" + to_string(i - entry.begin());
+		}
+		switch (type) {
+			case 0:
+				ch.vint.push_back(stoi(*i));
+				break;
+			case 1:
+				ch.vdouble.push_back(stod(*i));
+				break;
+			case 2:
+				ch.vstring.push_back(*i);
+				break;
+			default:
+				throw "Unexpected input type";
+		}
+		
+		columns.push_back(ch);
+	}
+}
+
+void Sheet::row_append(vector<string> &new_row) {
+	assert(new_row.size() == columns.size());
+	auto ch = columns.begin();
+	for (auto &r : new_row) {
+		switch (ch->flag) {
+			case 0:
+				ch->vint.push_back(stoi(r));
+				break;
+			case 1:
+				ch->vdouble.push_back(stod(r));
+				break;
+			case 2:
+				ch->vstring.push_back(r);
+				break;
+			default:
+				throw "Unexpected input type";
+		}
+		++ch;
+	}
+}
+
+void load_data(Sheet& sheet, const string& path, bool header) {
+	ifstream in(path);
+	
+	if (in.is_open())
+	{
+		string line;
+		istringstream iss;
+		vector<string> column_name;
+		if (header) { // first line is column name
+			if (!getline(in, line)) throw "No first line";
+			iss.str(line);
+			while (iss){
+				string s;
+				if (!getline( iss, s, ',' )) break;
+				column_name.push_back( trim(s) );
+			}
+		}
+		
+		if ( !getline(in, line) ) throw "No data";
+		iss.clear();
+		iss.str(line);
+		int t = 0;
+		vector<string> data;
+		while (iss){
+			string s;
+			if ( !getline( iss, s, ',' )) break;
+			if(!header) column_name.push_back("c" + to_string(t++));
+			data.push_back( trim(s) );
+		}
+		
+		Sheet sheet(data, column_name);
+		
+		while ( getline (in,line) ) {
+			iss.clear();
+			iss.str(line);
+			data.clear();
+			while (iss){
+				string s;
+				if ( !getline( iss, s, ',' )) break;
+				data.push_back( trim(s) );
+			}
+			sheet.row_append(data);
+		}
+		in.close();
+	}
+	else cout << "no file open" << endl;
+}
+
+int main (){
+	Sheet sh;
+	string s = "/Users/mcchu/Documents/16S-C++/sheet/sheet/test.txt";
+	load_data(sh, s, true);
+}
---
+++ libsheet.h
@@ -0,0 +1,92 @@
+// libsheet.h
+#include <iostream>
+#include <functional>
+#include <map>
+#include <string>
+#include <vector>
+#include <typeinfo>
+#include <bitset>
+using namespace std;
+
+class Sheet {
+public:
+	Sheet() {};
+	Sheet(vector<string>& entry, vector<string>& col_names);
+	Sheet(Sheet const &);
+	
+	template<typename Oper, size_t N, typename T>
+	bitset<N> filter(T column_index, Oper p) {
+		// Column index can be int or string(for column name)
+		// give column number, filter lambda expression, return a bitmap
+	}
+	
+	template<typename T, typename col_type>
+	void set(int y, col_type x, T value);
+	
+	
+	template<typename T>
+	Sheet get(int row, vector<T> cols);
+	
+	template<typename T>
+	Sheet get(int row, T col);
+	
+	template<typename T>
+	Sheet get(vector<int> rows, T cols);
+	
+	template<typename T>
+	Sheet get(vector<int> rows,  vector<T> cols);
+	
+	
+	void row_erase(int row);
+	void row_erase(vector<int> rows);
+	template<typename T>
+	void column_erase(T col);
+	
+	template<typename T>
+	void column_erase(vector<T> cols);
+	
+	void row_append(Sheet& new_sheet);
+	void row_append(vector<string> &new_row);
+	void column_append(Sheet& new_col);
+	
+	template<typename T>
+	void column_append(vector<T> new_col, string col_name);
+	
+	
+	
+private:
+	map<string, unsigned int>  column_map;  // column name to index
+	
+	class ColumnHead {
+		friend class Sheet;
+	public:
+		ColumnHead(string _column_name, int _flag) : column_name(_column_name), flag(_flag) {}
+		// 		template<typename T>
+		// 		void push_back(T input);
+		
+	private:
+		std::string column_name;
+		int flag; // int:0 double:1 string:2
+		
+		vector<int> vint;
+		vector<double> vdouble;
+		vector<std::string> vstring;
+		/* other possible field */
+		
+	};
+	vector<ColumnHead> columns;
+	
+};
+
+//vector<int> bit2int(bitset& filter_result);
+
+void load_data(Sheet& sheet, const string& path, bool header = true);
+void dump_data(Sheet& sheet, const string& file_path, bool header = true);
+
+void row_shuffle(Sheet& sheet, vector<int>& ref);
+
+template <typename T>
+void sort(Sheet& sheet, T cols, bool descend = true);
+
+//template <typename T, typename lambda_input>
+//void apply(Sheet& sheet, T column_num, function<bool (lambda_input)> func)
