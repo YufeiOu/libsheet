1fc3168abe44cc938e69634459931aac75a9e92b
Yufei Ou <yo2265@columbia.edu>
2016-04-28 22:14:52 -0400

    fix libsheet.h

--- libsheet.h
+++ libsheet.h
@@ -127,7 +127,7 @@ public:
 
     int col_len() {
     	if (!columns.size()) return 0;
-    	return max(max(columns.at(0).vint.size(), columns.at(0).vdouble.size()), columns.at(0).vstring.size();
+    	return max(max(columns.at(0).vint.size(), columns.at(0).vdouble.size()), columns.at(0).vstring.size());
     }
 
     int row_len() { return columns.size(); }
